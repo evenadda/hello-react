cb408944c26681eab7752683547dd7bc1d11402a
nadda <nadda@mfec.co.th>
2020-06-08 11:20:59 +0700

    first commit

---
+++ src/MyStatelessComponent.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const MyStatelessComponent = (props) => (
+    <div>
+        <h1>{props.title}</h1>
+        <h1>{props.message}</h1>
+    </div>
+)
+ 
+export default MyStatelessComponent;
